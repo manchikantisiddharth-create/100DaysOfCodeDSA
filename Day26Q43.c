2a6e085710b92a4f10b06189e2575ece71246044
Siddharth Manchikanti <manchikantisiddharth@gmail.com>
2026-02-26 18:46:40 +0530

    Day26

--- Day26Q43.c
+++ Day26Q43.c
@@ -17,11 +17,18 @@ struct Node {
 
 struct Node* insertEnd(struct Node* head, int value) {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    
+    if (newNode == NULL) {
+        printf("Memory allocation failed!\n");
+        return head;
+    }
+
     newNode->data = value;
     newNode->prev = NULL;
     newNode->next = NULL;
 
     if (head == NULL) {
+        printf("Inserted %d as head node\n", value);
         return newNode;
     }
 
@@ -34,22 +41,35 @@ struct Node* insertEnd(struct Node* head, int value) {
     temp->next = newNode;
     newNode->prev = temp;
 
+    printf("Inserted %d at the end\n", value);
+
     return head;
 }
+
 void printList(struct Node* head) {
     struct Node* temp = head;
+
+    if (temp == NULL) {
+        printf("The list is empty.\n");
+        return;
+    }
+
+    printf("Doubly Linked List (Forward Traversal): ");
     while (temp != NULL) {
         printf("%d ", temp->data);
         temp = temp->next;
     }
+    printf("\n");
 }
 
 int main() {
     int n, value;
     struct Node* head = NULL;
 
+    printf("Enter number of elements: ");
     scanf("%d", &n);
 
+    printf("Enter %d space-separated integers:\n", n);
     for (int i = 0; i < n; i++) {
         scanf("%d", &value);
         head = insertEnd(head, value);
