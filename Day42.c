afd80fc6c1cc2016877240def619a0362b598629
Siddharth Manchikanti <manchikantisiddharth@gmail.com>
2026-03-15 09:31:20 +0530

    Day42 43

---
+++ Day42.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#define MAX 100
+
+int queue[MAX], front = 0, rear = -1;
+int stack[MAX], top = -1;
+
+void enqueue(int x) {
+    queue[++rear] = x;
+}
+
+int dequeue() {
+    return queue[front++];
+}
+
+void push(int x) {
+    stack[++top] = x;
+}
+
+int pop() {
+    return stack[top--];
+}
+
+int main() {
+    int n, i, x;
+
+    scanf("%d", &n);
+
+    // Input elements into queue
+    for(i = 0; i < n; i++) {
+        scanf("%d", &x);
+        enqueue(x);
+    }
+
+    // Move queue elements to stack
+    for(i = 0; i < n; i++) {
+        push(dequeue());
+    }
+
+    // Move stack elements back to queue
+    for(i = 0; i < n; i++) {
+        enqueue(pop());
+    }
+
+    // Print reversed queue
+    for(i = front; i <= rear; i++) {
+        printf("%d ", queue[i]);
+    }
+
+    return 0;
+}
